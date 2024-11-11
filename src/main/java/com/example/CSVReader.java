a93ad7e57ff49e8df27ac41212e043a39245c1b0
JoaoOliveira <j.maria.t.oliveira@gmail.com>
2024-11-11 19:18:10 +0000

    Continuação do desenvolvimento da class CSVReader

--- src/main/java/com/example/CSVReader.java
+++ src/main/java/com/example/CSVReader.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import java.util.ArrayList;
 import java.io.IOException;
 import java.util.List;
 import java.io.Reader;
@@ -10,12 +11,43 @@ import org.apache.commons.csv.CSVRecord;
 
 public class CSVReader {
     private String csvFilePath;
+    private List<CSVRecord> data;
 
+    //CSVReader constructor
     public CSVReader(String csvFilePath) {
         this.csvFilePath = csvFilePath;
+        data = new ArrayList<CSVRecord>();
     }
 
-    public void readCSV() {
+
+       //Method that stores de information read in the csv file in the variable data, making it accessible whenever we want to use it without having to read the file again
+
+       public void readCSV() {
+        try (Reader reader = new FileReader(csvFilePath);
+                CSVParser csvParser = new CSVParser(reader,
+                        CSVFormat.DEFAULT.withDelimiter(';').withFirstRecordAsHeader())) {
+
+            // Adding the read values to the variable data
+            for (CSVRecord csvRecord : csvParser) {
+                data.add(csvRecord);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //getting a record by its id
+    public CSVRecord getRecordByOBJECTID(int objId){
+        if(objId < 0 || objId >= data.size()){
+            System.out.println("Invalid object id! Id doesn't exist");
+            return null;
+        }
+        return data.get(objId);
+    }
+
+
+    //Debug method used to see if the file is being read correctly
+    public void printCSV(){
         try (Reader reader = new FileReader(csvFilePath);
                 CSVParser csvParser = new CSVParser(reader,
                         CSVFormat.DEFAULT.withDelimiter(';').withFirstRecordAsHeader())) {
@@ -24,7 +56,7 @@ public class CSVReader {
             List<String> headers = csvParser.getHeaderNames();
             System.out.println("Headers: " + headers);
 
-            // Iterate through the records
+            // Outputting the records
             for (CSVRecord csvRecord : csvParser) {
                 // Accessing values by the names assigned to each column
                 String objectId = csvRecord.get("OBJECTID");
@@ -34,7 +66,7 @@ public class CSVReader {
                 String shapeArea = csvRecord.get("Shape_Area");
                 String geometry = csvRecord.get("geometry");
                 String owner = csvRecord.get("OWNER");
-
+ 
                 System.out.println("Record No - " + csvRecord.getRecordNumber());
                 System.out.println("OBJECTID: " + objectId);
                 System.out.println("PAR_ID: " + parId);
@@ -49,4 +81,18 @@ public class CSVReader {
             e.printStackTrace();
         }
     }
+
+    //main for debugging the class
+    public static void main(String [] args){
+        CSVReader test = new CSVReader("C:/Users/jmari/OneDrive/Ambiente de Trabalho/ProjEngSoft/ES-2024-1Sem-LETI-GrupoI/Madeira-Moodle.csv");
+        //printCSV() its working
+        //test.printCSV()
+        
+        //readCSB() its working
+        test.readCSV();
+
+        //getRecordByOBJECTID() its working
+        System.out.println(test.getRecordByOBJECTID(1).get("PAR_ID")); //getting the PAR_ID of the object with an id 16999
+    }
+ 
 }
