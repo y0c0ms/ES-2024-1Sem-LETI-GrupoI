277404d71620d9cfd9d05a176ddc110dc59feac6
y0c0ms <majpr@iscte-iul.pt>
2024-11-12 04:25:15 +0000

    Update para executar tudo na Main

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,9 +1,13 @@
 package com.example;
 
+import javafx.application.Application;
+
 public class Main {
     public static void main(String[] args) {
         CSVReader csvReader = new CSVReader();
         csvReader.readCSV();
         // csvReader.printCSV(); //Outputs the readings to the console
+        Application.launch(AdjacencyCheckerGUI.class, args);
+        // Application.launch(PropertyMapVisualizer.class, args);
     }
 }
